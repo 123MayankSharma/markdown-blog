35b0cb198ac28fc48480434c76544cee52fb8035
123MayankSharma <Mayanksharma922001@gmail.com>
2021-10-06 18:00:38 +0530

    added basic layout and structure base

---
+++ pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h2>about Page</h2>
+        </div>
+    )
+}
+
+export default About
+
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import 'tailwindcss/tailwind.css'
-
+import Link from 'next/link'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (<>
+        <nav>
+            <ul>
+                <li>
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/About">
+                        <a>About</a>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+        <Component {...pageProps} />
+
+    </>)
 }
 
 export default MyApp
